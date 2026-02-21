4f27e841db4c2a75f3bfd6acaa0c6df197201ef6
Barbaaz <paulocarneiro93@hotmail.com>
2026-02-21 03:21:06 +0000

    Added router to main.py

--- app/main.py
+++ app/main.py
@@ -4,3 +4,7 @@ from app.routers import data_router
 app = FastAPI(title="Lab Data Processor API")
 
 app.include_router(data_router.router)
+
+@app.get("/")
+def root():
+    return {"message": "Lab Data Processor API is running"}
